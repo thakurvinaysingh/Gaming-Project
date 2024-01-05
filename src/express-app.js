1ab259e287b5e928f565b20b1de43d1a59af1d6f
thakurvinaysingh <vinay.singh5497@gmail.com>
2024-01-05 15:47:06 +0530

    one-22 commit

--- src/express-app.js
+++ src/express-app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const {user} = require("./api")
-
+const cookieParser = require('cookie-parser')
 const HandleError = require("./utils/error-handler")
 
 module.exports =async(app)=>{
+    app.use(cookieParser());
     app.use(express.json({limit:'1mb'}));
     app.use(express.urlencoded({extended:true,limit:'1mb'}));
-    app.use(cors());
+    app.use(cors({
+        credentials: 'true',
+        origin: 'http://localhost:3000'
+      }));
+      
     app.use(express.static(__dirname + '/public'));
 
     //api handle
